a1bccded8312e5fc84fa6059905b054ebb852484
playsees <124029605+playsees@users.noreply.github.com>
2023-10-19 09:33:40 +0700

    Update dictionary2.0.c

--- dictionary2.0.c
+++ dictionary2.0.c
@@ -4,7 +4,6 @@
 #define MAX_WORDS 1000
 #define MAX_WORD_LEN 50
 
-// Определяем структуру данных для хранения словаря
 struct dict_entry {
     char word[MAX_WORD_LEN];
     char translation[MAX_WORD_LEN];
@@ -16,38 +15,41 @@ int main() {
     char output_filename[] = "output.txt";
     char line[1000];
     char *word;
-    int i, j, k, n, found;
+    int i, j, n, found;
     struct dict_entry dict[MAX_WORDS];
     int freq[MAX_WORDS] = {0};
 
-    // Определяем словарь статически, используя заданные слова и их переводы
+    // Define dictionary
     struct dict_entry dict[] = {
         {"тирг", "tirg"},
         {"взор", "vzor"}
     };
     n = sizeof(dict) / sizeof(dict[0]);
 
-    // Открываем входной файл для чтения и выходной файл для записи
+    // Open input file
     input_file = fopen(input_filename, "r");
     if (input_file == NULL) {
         printf("Error opening input file\n");
         return 1;
     }
+
+    // Open output file
     output_file = fopen(output_filename, "w");
     if (output_file == NULL) {
         printf("Error opening output file\n");
         return 1;
     }
 
-    // Считываем входной файл построчно и разбиваем каждую строку на слова
+    // Read input file line by line
     while (fgets(line, sizeof(line), input_file)) {
+        // Parse line into words
         word = strtok(line, " ,.-\n");
         while (word != NULL) {
-            // Ищем слово в словаре
+            // Search for word in dictionary
             found = 0;
             for (i = 0; i < n; i++) {
                 if (strcmp(word, dict[i].word) == 0) {
-                    // Слово найдено в словаре, заменяем его переводом
+                    // Word found in dictionary, replace with translation
                     fprintf(output_file, "%s ", dict[i].translation);
                     freq[i]++;
                     found = 1;
@@ -55,7 +57,7 @@ int main() {
                 }
             }
             if (!found) {
-                // Слово не найдено в словаре, записываем его без изменений
+                // Word not found in dictionary, keep original word
                 fprintf(output_file, "%s ", word);
             }
             word = strtok(NULL, " ,.-\n");
@@ -63,11 +65,11 @@ int main() {
         fprintf(output_file, "\n");
     }
 
-    // Сортируем словарь по частоте появления слов
+    // Sort dictionary by frequency of occurrence
     for (i = 0; i < n - 1; i++) {
         for (j = i + 1; j < n; j++) {
             if (freq[i] < freq[j]) {
-                // Меняем местами элементы словаря и частоты
+                // Swap entries
                 struct dict_entry temp = dict[i];
                 dict[i] = dict[j];
                 dict[j] = temp;
@@ -78,14 +80,15 @@ int main() {
         }
     }
 
-    // Выводим отсортированный словарь на консоль
+    // Print sorted dictionary to console
     printf("Dictionary:\n");
     for (i = 0; i < n; i++) {
         printf("%s -> %s (%d)\n", dict[i].word, dict[i].translation, freq[i]);
     }
 
-    // Закрываем файлы и завершаем программу
+    // Close files
     fclose(input_file);
     fclose(output_file);
+
     return 0;
 }
